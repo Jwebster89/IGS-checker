92d77fe54f798d6d31406a049a8b17a23494eb37
Jwebster89 <45958589+Jwebster89@users.noreply.github.com>
2019-02-06 16:13:32 +1100

    Add files via upload
    Added a python + R script to cluster repeats based on a multifasta file

---
+++ R_IGS_phylo.R
@@ -0,0 +1,14 @@
+library(rjson)
+library(plyr)
+setwd("C:/Users/Mbac/Documents/Projects/Vert/Projects/")
+
+json_file <- "R_VCG_phyl_input.txt"
+json_data <- fromJSON(file= json_file)
+asFrame <- do.call("rbind.fill", lapply(json_data, as.data.frame))
+asFrame[is.na(asFrame)] <- 0
+
+asFrame_rownames <- asFrame[,-1]
+rownames(asFrame_rownames) <- asFrame[,1]
+
+
+plot(hclust(dist(asFrame_rownames),method="average"))
